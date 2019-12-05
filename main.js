a99bc1400edc14fa97db50a3101bfe9d4655f662
Pedro Carreira <pcarreira@alter-solutions.com>
2019-12-05 17:06:03 +0000

    Create auto-updating app

---
+++ main.js
@@ -0,0 +1,57 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+let mainWindow;
+
+function createWindow () {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+    mainWindow.loadFile('index.html');
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', () => {
+    createWindow();
+    //check if there are any available updates, if so, they'll be auto downloaded
+    autoUpdater.checkForUpdatesAndNotify();
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+//Added for getting the version!
+ipcMain.on('app_version', (event) => {
+    event.sender.send('app_version', { version: app.getVersion() });
+});
+
+//Add listeners to handle update events, if so a message is sent in index.html!
+// Once it downloads, we’ll send another message notifying them that
+// the update will be installed when they quit the app.
+autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update_downloaded');
+});
+
+// Event listener that will install the new version if the user selects 'Restart';
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
+});
