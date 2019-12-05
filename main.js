87865208d3a33ad6deb7e8812f7dd6cebe3ad903
Pedro Carreira <pcarreira@alter-solutions.com>
2019-12-05 17:42:53 +0000

    Version 1.0.3 update

--- main.js
+++ main.js
@@ -46,7 +46,6 @@ ipcMain.on('app_version', (event) => {
 autoUpdater.on('update-available', () => {
     mainWindow.webContents.send('update_available');
 });
-
 autoUpdater.on('update-downloaded', () => {
     mainWindow.webContents.send('update_downloaded');
 });
